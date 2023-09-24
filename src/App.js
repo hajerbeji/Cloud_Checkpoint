6efaa03c9b034272d64b1ca70111f797c606543f
Hajer Beji <hajer.lasmar.beji.05@gmail.com>
2023-09-24 10:58:20 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleMap from './components/Gmaps';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleMap />
+    </div>
+  );
+}
+
+export default App;
